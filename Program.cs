ec9b68030b5d851f2e4420c3bd2a328dc71e9256
lgc2 <lgustavoccarvalho@gmail.com>
2023-12-23 19:34:28 -0300

    feat: adiciona aprendizado de listas E tipos genéricos

--- Program.cs
+++ Program.cs
@@ -2,16 +2,49 @@
 {
     static void Main()
     {
-        ILogger logger = new FileLogger("myLog.txt");
-        BankAccount account1 = new BankAccount("LG", 100, logger);
-        BankAccount account2 = new BankAccount("Letícia", 150, logger);
+        // ILogger logger = new FileLogger("myLog.txt");
+        // BankAccount account1 = new BankAccount("LG", 100, logger);
+        // BankAccount account2 = new BankAccount("Letícia", 150, logger);
+
+        var logger = new FileLogger("myLog.txt");
+        var account1 = new BankAccount("LG", 100, logger);
+        var account2 = new BankAccount("Letícia", 150, logger);
 
         account1.Deposit(-100);
         Console.WriteLine($"O saldo do(a) {account1.Name} é R${account1.Balance}");
 
         account2.Deposit(100);
         Console.WriteLine($"O saldo do(a) {account2.Name} é R${account2.Balance}");
+
+        List<BankAccount> accounts = new List<BankAccount>()
+        {
+            account1
+        };
+
+        accounts.Add(account2);
+
+        foreach (BankAccount account in accounts)
+        {
+            Console.WriteLine(account.Name);
+        }
+
+        List<int> numbers = new List<int> { 1, 2, 4, 8, 10 };
+
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        DataStore<int> store = new DataStore<int>();
+        Console.WriteLine($"store antes da edição de valor: {store.Value}");
+        store.Value = 37;
+        Console.WriteLine($"store após edição de valor: {store.Value}");
     }
+}
+
+class DataStore<T>
+{
+    public T Value { get; set; }
 }
 
 class FileLogger : ILogger
